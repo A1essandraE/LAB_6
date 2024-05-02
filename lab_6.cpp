a35ca93ff93b9da232d1ecbb9b62a51f89f66091
alesnykh22@mail.ru <alesnykh22@mail.ru>
2024-05-02 11:28:37 +0300

    commit

--- lab_6.cpp
+++ lab_6.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vettore = { 23, 4, 190, 34, 0, 44, 98, 1, 33, 75 };
+
+    std::ostream_iterator<int> it(std::cout, " ");
+    //output
+    std::cout << "output: ";
+    for (auto it = vettore.begin(); it != vettore.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    //reverse output
+    std::cout << "reversed output: ";
+    std::copy(vettore.rbegin(), vettore.rend(), it);
+    std::cout << std::endl;
+
+    //min element
+    std::vector<int>::iterator min = std::min_element(vettore.begin(), vettore.end());
+    std::cout << "min element: " << *min << std::endl;
+
+    //sorting in descending order
+    std::sort(vettore.begin(), vettore.end(), std::greater<int>());
+    std::cout << "output: ";
+    std::copy(vettore.begin(), vettore.end(), it);
+    std::cout << std::endl;
+
+    //sorting in ascending order
+    std::sort(vettore.begin(), vettore.end(), std::less<int>());
+    std::cout << "output: ";
+    std::copy(vettore.begin(), vettore.end(), it);
+    std::cout << std::endl;
+
+    vettore.push_back(8);
+    std::cout << "output: ";
+    for (auto it = vettore.begin(); it != vettore.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
 
-int main{
 
-	std::vector<int> vettore = {3, 65, 34, 138, 25};
-    
-std::ostream_iterator<int> it(std::cout, ", ");
-    for (vettore.begin(), vettore.end()
-    std::cout << *it << std::endl;
-
-	return 0;
+    return 0;
 }
