8050377ab0ca080cc574a69dcdaa563027aa4e64
alesnykh22@mail.ru <alesnykh22@mail.ru>
2024-05-02 11:28:12 +0300

    commit

--- lab_6.cpp
+++ lab_6.cpp
@@ -6,8 +6,9 @@ int main{
 
 	std::vector<int> vettore = {3, 65, 34, 138, 25};
     
-    std::ostream_iterator<int> vettore_it;
-    std::cout << *vettore_it << std::endl;
+std::ostream_iterator<int> it(std::cout, ", ");
+    for (vettore.begin(), vettore.end()
+    std::cout << *it << std::endl;
 
 	return 0;
 }
