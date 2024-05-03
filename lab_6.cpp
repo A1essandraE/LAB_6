7e5c4e87972dc610796d243b1661efdbae971c43
alesnykh22@mail.ru <alesnykh22@mail.ru>
2024-05-03 18:14:09 +0300

    commit

--- lab_6.cpp
+++ lab_6.cpp
@@ -2,16 +2,21 @@
 #include <vector>
 #include <algorithm>
 
+void output(std::vector<int> vector) {
+    for (auto it = vector.begin(); it != vector.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
-    std::vector<int> vettore = { 23, 4, 190, 34, 0, 44, 98, 1, 33, 75 };
+    std::vector<int> vettore = { 23, 4, 190, 34, 0, 44, 98, 1, 33, 75, 34, 93, 27, 3, 1, 9, 9, 27 };
 
     std::ostream_iterator<int> it(std::cout, " ");
     //output
     std::cout << "output: " << std::endl;;
-    for (auto it = vettore.begin(); it != vettore.end(); ++it) {
-        std::cout << *it << " ";
-    }
+    std::copy(vettore.begin(), vettore.end(), it);
     std::cout << std::endl;
 
     //reverse output
@@ -22,6 +27,16 @@ int main()
     //min element
     std::vector<int>::iterator min = std::min_element(vettore.begin(), vettore.end());
     std::cout << "min element: " << *min << std::endl;
+
+    //erasing all even numbers
+    for (std::vector<int>::iterator it = vettore.begin(); it != vettore.end(); ) {
+        if (*it % 2 == 0)
+            it = vettore.erase(it);
+        else
+            ++it;
+    }
+    std::cout << "output after erasing all even elements: " << std::endl;
+    output(vettore);
 
     //sorting in descending order
     std::sort(vettore.begin(), vettore.end(), std::greater<int>());
@@ -42,6 +57,18 @@ int main()
     }
     std::cout << std::endl;
 
+    //index of an added element
+    std::vector<int>::iterator elem = std::find(vettore.begin(), vettore.end(), 8);
+    if (elem != vettore.end())
+        std::cout << "the element is found at index [" << std::distance(vettore.begin(), elem) << "]" << std::endl;
+    else
+        std::cout << "the element is not found" << std::endl;
+
+    //deleting all the duplicates
+    auto unique = std::unique(vettore.begin(), vettore.end());
+    vettore.resize(std::distance(vettore.begin(), unique));
+    std::cout << "output after removing all duplicates: " << std::endl;
+    output(vettore);
 
     return 0;
 }
